44ce6be64dce797d4934cccd5406cd625a152049
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2024-06-12 23:14:01 -0700

    FL and BR modules need a hardware fix

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WhatIsTheFrontTest.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class WhatIsTheFrontTest extends LinearOpMode {
+    DcMotor FLR;
+    DcMotor FLL;
+    DcMotor FRL;
+    DcMotor FRR;
+    DcMotor BLR;
+    DcMotor BLL;
+    DcMotor BRR;
+    DcMotor BRL;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        FLR = hardwareMap.get(DcMotor.class, "FLR");
+        FLL = hardwareMap.get(DcMotor.class, "FLL");
+        //problem with the FL gotube part skipping
+        FRL = hardwareMap.get(DcMotor.class, "FRL");
+        FRR = hardwareMap.get(DcMotor.class, "FRR");
+        BLR = hardwareMap.get(DcMotor.class, "BLR");
+        BLL= hardwareMap.get(DcMotor.class, "BLL");
+        //BL is good
+        BRR= hardwareMap.get(DcMotor.class, "BRR");
+        BRL= hardwareMap.get(DcMotor.class, "BRL");
+        FLR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        FLL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        FRL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        FRR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        BRR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        BRL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        BLL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        BLR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        waitForStart();
+        while(opModeIsActive()){
+//            BRL.setPower(gamepad1.left_trigger);
+//            BRR.setPower(gamepad1.left_trigger);
+        }
+    }
+}
